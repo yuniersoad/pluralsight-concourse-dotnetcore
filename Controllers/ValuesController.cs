9324530c31ce5e1d00dbfa022da1d4184353c1a7
rseroter <rseroter@hotmail.com>
2019-07-24 10:02:29 -0700

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pluralsight_concourse_dotnetcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "pluralsight", "pivotal" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "pluralsight";
+        }
+
+        // GET api/values/5/status
+        [HttpGet("{id}/status")]
+        public string GetCaseStatus(int id)
+        {
+            if(id > 0 && id <= 10) {
+            return "closed";
+            }
+            else { 
+                return "open";
+            }
+        }
+
+        
+
+    }
+}
---
+++ TestClass.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using pluralsight_concourse_dotnetcore.Controllers;
+
+namespace unittests {
+
+    public class TestClass {
+
+        private ValuesController _vc;
+
+        public TestClass() {
+            _vc = new ValuesController();
+        }
+
+        [Fact]
+        public void Test1(){
+            
+            Assert.Equal("pluralsight", _vc.Get(2).Value);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(3)]
+        [InlineData(9)]
+        //[InlineData(20)]
+        public void Test2(int value) {
+            Assert.Equal("closed", _vc.GetCaseStatus(value));
+        }
+
+    }
+
+}
